4b2c84db4b4cac25316cf5f3518d45ad61d7288b
junandahanif2406-sys <junandahanif2406@gmail.com>
2026-09-22 16:49:07 +0700

    Initial commit

---
+++ hello.php
@@ -0,0 +1,15 @@
+!DOCTYPE html>
+<html lang="id">
+    <head>
+        <meta charset="UTF-8">
+        <title>Kursusin</title>
+    </head>
+    <body>
+        <h1>Selamat datang di Kursusin</h1>
+        <p>Temukan kursus terbaik untukmu.</p>
+        <img src="logo.png" alt="Logo Kursusin" class="img-fluid"
+        style="max-height: 200px;"><a
+        href="pratikum-1.html">Pratikum-1
+    </a>
+    </body>
+    </html>
---
+++ server-time.php
@@ -0,0 +1,4 @@
+<?php
+$serverTime = date('Y-m-d H:i:s');
+echo 'waktu server: ' . $serverTime;
+?>
